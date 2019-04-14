04f9f46b75ad67b189d457a30da4dbac348d6774
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-14 10:53:02 +0100

    Creating a Layout component
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/13556476?start=0

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
-import './App.css';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Burger Builder</h1>
-			</div>
+			<Layout>
+				<p>Burger Builder</p>
+			</Layout>
 		);
 	}
 }

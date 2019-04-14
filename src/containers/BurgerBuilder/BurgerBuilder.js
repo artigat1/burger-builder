d91408263c37b64f882338b51a0b396ee924c1db
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-14 13:07:08 +0100

    Removing ingredients safely
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8109030?start=0

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -38,13 +38,44 @@ class BurgerBuilder extends Component {
 		});
 	};
 
-	removeIngredientHandler = (type) => {};
+	removeIngredientHandler = (type) => {
+		const oldCount = this.state.ingredients[type];
+		if (oldCount <= 0) {
+			return;
+		}
+
+		const updatedCount = oldCount - 1;
+		const updatedIngredients = {
+			...this.state.ingredients
+		};
+		updatedIngredients[type] = updatedCount;
+
+		const priceDeduction = INGREDIENT_PRICES[type];
+		const newPrice = this.state.totalPrice - priceDeduction;
+
+		this.setState({
+			ingredients: updatedIngredients,
+			totalPrice: newPrice
+		});
+	};
 
 	render() {
+		const disabledInfo = {
+			...this.state.ingredients
+		};
+		console.log('disabledInfo', disabledInfo);
+		for (let key in disabledInfo) {
+			disabledInfo[key] = disabledInfo[key] <= 0;
+		}
+
 		return (
 			<Fragment>
 				<Burger ingredients={this.state.ingredients} />
-				<BuildControls ingredientAdded={this.addIngredientHandler} />
+				<BuildControls
+					ingredientAdded={this.addIngredientHandler}
+					ingredientRemoved={this.removeIngredientHandler}
+					disabled={disabledInfo}
+				/>
 			</Fragment>
 		);
 	}

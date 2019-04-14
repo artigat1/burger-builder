3b295c209f5c9db6600352a021487117e75ac6b3
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-14 12:27:43 +0100

    Calculating the ingredient sum dynamically
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8109020?start=0

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -7,11 +7,19 @@ const burger = (props) => {
 	// We get an object and not an array.
 	// First create an empty array with the correct number of elements from the object
 	// The key is the name of the ingredient and must match the type in the switch statement in BurgerIngredient
-	const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-		return [ ...Array(props.ingredients[igKey]) ].map((_, idx) => {
-			return <BurgerIngredient key={igKey + idx} type={igKey} />;
-		});
-	});
+	let transformedIngredients = Object.keys(props.ingredients)
+		.map((igKey) => {
+			return [ ...Array(props.ingredients[igKey]) ].map((_, idx) => {
+				return <BurgerIngredient key={igKey + idx} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+
+	if (transformedIngredients.length === 0) {
+		transformedIngredients = <p>Please start adding ingredients.</p>;
+	}
 
 	return (
 		<div className={classes.Burger}>

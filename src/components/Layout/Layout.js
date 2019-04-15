0bd00e6afa5ad296c0731a7cba7469fb99bc4c08
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-15 11:52:36 +0100

    Creating a responsive side drawer
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8125740?start=0
    
    This is actually 3 classes!

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react';
 
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
 	<Fragment>
 		<Toolbar />
+		<SideDrawer />
 		<div>Toolbar, Side-drawer, Backdrop</div>
 		<main className={classes.Content}>{props.children}</main>
 	</Fragment>

d90233e53428419306c97348da30d55d9e0e8702
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-15 10:20:41 +0100

    Showing & hiding the modal
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8109040?start=0

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
 			meat: 0
 		},
 		totalPrice: 4,
-		purchaseable: false
+		purchaseable: false,
+		purchasing: false
 	};
 
 	updatePurchaseState(ingredients) {
@@ -73,6 +74,12 @@ class BurgerBuilder extends Component {
 		this.updatePurchaseState(updatedIngredients);
 	};
 
+	purchaseHandler = () => {
+		this.setState({
+			purchasing: true
+		});
+	};
+
 	render() {
 		const disabledInfo = {
 			...this.state.ingredients
@@ -84,7 +91,7 @@ class BurgerBuilder extends Component {
 
 		return (
 			<Fragment>
-				<Modal>
+				<Modal show={this.state.purchasing}>
 					<OrderSummary ingredients={this.state.ingredients} />
 				</Modal>
 				<Burger ingredients={this.state.ingredients} />
@@ -94,6 +101,7 @@ class BurgerBuilder extends Component {
 					disabled={disabledInfo}
 					price={this.state.totalPrice}
 					purchaseable={this.state.purchaseable}
+					ordered={this.purchaseHandler}
 				/>
 			</Fragment>
 		);

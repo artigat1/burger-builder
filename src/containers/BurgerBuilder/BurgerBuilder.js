c699451dce2d9073b0331592c23b231b18ef2625
Steve Doggett <artigat1@users.noreply.github.com>
2019-04-22 19:30:32 +0100

    Redux & the UI state
    https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/t/lecture/8211926?start=0

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,6 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component {
     state = {
-        purchaseable: false,
         purchasing: false,
         loading: false,
         error: false,
@@ -31,9 +30,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({
-            purchaseable: sum > 0,
-        });
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -88,7 +85,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
                         price={this.props.price}
-                        purchaseable={this.state.purchaseable}
+                        purchaseable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                     />
                 </Fragment>
